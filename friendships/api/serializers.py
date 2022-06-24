b1ea84536cf2c4f445317e92b2bed305e4e57734
Yue Ma <plenilune.m@gmail.com>
2022-06-24 13:05:40 -0700

    follow & unfollow API

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from accounts.api.serializers import UserSerializerForFriendship
 from friendships.models import Friendship
@@ -18,3 +19,33 @@ class FollowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friendship
         fields = ('user', 'created_at')
+
+class FriendshipSerializerForCreate(serializers.ModelSerializer):
+    from_user_id = serializers.IntegerField()
+    to_user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Friendship
+        fields = ('from_user_id', 'to_user_id')
+
+    def validate(self, attrs):
+        if attrs['from_user_id'] == attrs['to_user_id']:
+            raise ValidationError({
+                'message': 'You cannot follow yourself.',
+            })
+        if Friendship.objects.filter(
+            from_user_id=attrs['from_user_id'],
+            to_user_id=attrs['to_user_id'],
+        ).exists():
+            raise ValidationError({
+                'message': 'You has already followed this user.'
+            })
+        return attrs
+
+
+    def create(self, validated_data):
+        return Friendship.objects.create(
+            from_user_id=validated_data['from_user_id'],
+            to_user_id=validated_data['to_user_id'],
+        )
+
--- friendships/api/views.py
+++ friendships/api/views.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from friendships.api.serializers import FollowerSerializer, FollowingSerializer
+from friendships.api.serializers import FollowerSerializer, FollowingSerializer, FriendshipSerializerForCreate
 from friendships.models import Friendship
 
 
 class FriendshipViewSet(viewsets.GenericViewSet):
+    serializer_class = FriendshipSerializerForCreate
     queryset = User.objects.all()
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
@@ -24,6 +25,40 @@ class FriendshipViewSet(viewsets.GenericViewSet):
         friendships = Friendship.objects.filter(from_user_id=pk).order_by('-created_at')
         serializer = FollowingSerializer(friendships, many=True)
         return Response({'followers': serializer.data}, status=200)
+
+    @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
+    def follow(self, request, pk):
+        serializer = FriendshipSerializerForCreate(data={
+            'from_user_id': request.user.id,
+            'to_user_id': pk,
+        })
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check the input.',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        instance = serializer.save()
+        return Response(
+            FollowingSerializer(instance).data,
+            status=status.HTTP_201_CREATED
+        )
+
+    @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
+    def unfollow(self, request, pk):
+        unfollow_user = self.get_object()
+        if request.user.id == unfollow_user.id: # pk is string format 1 != '1'
+            return Response({
+                'success': False,
+                'message': 'You cannot unfollow yourself.',
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        deleted, _ = Friendship.objects.filter(
+            from_user=request.user,
+            to_user=unfollow_user,
+        ).delete()
+        return Response({'success': True, 'delete': deleted})
+
 
     def list(self, request):
         return Response({'message': 'This is friendships page.'})
